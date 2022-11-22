d357bc08541fb507fde336cdd3de9ccb847e585c
Kyle Wong <knw@ucsb.edu>
2022-11-21 17:46:16 -0800

    updated topology function and added file generator

--- data_generator.cpp
+++ data_generator.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <string>
 #include <queue>
+#include <unordered_map>
 #include <time.h>
+#include <fstream>
 using namespace std;
 
-int TAXA = 10; // set number of taxa
-int LENGTH = 1000; // set length of dna sequence
+const int TAXA = 10; // set number of taxa
+const int LENGTH = 1000; // set length of dna sequence
 
 // Only use for setting the initial char, choose U.A.R
 char getRandomChar() {
@@ -54,9 +56,43 @@ vector<vector<int>> createTopology(int n) {
     return adjList;
 }
 
+// Better tree topology generator
+vector<vector<int>> createTopology2(int n) {
+    vector<vector<int>> adjList(2 * n - 1); // total # of nodes will be 2n-1
+    vector<int> leaves;
+    leaves.push_back(0);
+    int count = 0;
+    while (leaves.size() < n) {
+        // randomly pick a leaf to split
+        int index = rand() % (int)leaves.size(); 
+        int parent = leaves.at(index);
+        leaves.erase(leaves.begin() + index); // O(n) x_x
+        // add left child
+        leaves.push_back(++count);
+        adjList.at(parent).push_back(count);
+        adjList.at(count).push_back(parent);
+        // add right child
+        leaves.push_back(++count);
+        adjList.at(parent).push_back(count);
+        adjList.at(count).push_back(parent);
+    }
+    return adjList;
+}
+
+// Gets a non-leaf node to start 
+int getStartingNode(vector<vector<int>>& adjList) {
+    int res = rand() % (int)adjList.size();
+    while (adjList.at(res).size() == 1) {
+        res = rand() % (int)adjList.size();
+    }
+    return res;
+}
+
 // Input: Tree topology, starting node, sequence length
 // Output: DNA sequences for all leaf nodes
-vector<string> generateData(vector<vector<int>>& adjList, int start, int length) {
+vector<string> generateData(vector<vector<int>>& adjList, int length) {
+    // int start = getStartingNode(adjList); Bugged?
+    int start = 0;
     vector<string> res(adjList.size(), "");
     for (int i=0; i<length; ++i) {
         queue<int> queue;
@@ -71,11 +107,22 @@ vector<string> generateData(vector<vector<int>>& adjList, int start, int length)
             for (int j=0; j<adjList.at(curr).size(); ++j) {
                 int neighbor = adjList.at(curr).at(j);
                 if (!visited.at(neighbor)) {
-                    c = getRandomCharJC(0.01, res.at(curr).back()); // modify substituion rates here, rate = [0, 0.25]
+                    c = getRandomCharJC(0.1, res.at(curr).back()); // modify substituion rates here, rate = [0, 0.25]
                     res.at(neighbor).push_back(c);
                     queue.push(neighbor);
                 }
             }
+        }
+    }
+    return res;
+}
+
+// Extracts <leaf node, DNA> from tree topology and DNA sequences
+unordered_map<int, string> getLeaves(vector<vector<int>>& adjList, vector<string>& data) {
+    unordered_map<int, string> res;
+    for (int i=0; i<adjList.size(); ++i) {
+        if (adjList.at(i).size() == 1) {
+            res.insert({i, data.at(i)});
         }
     }
     return res;
@@ -102,23 +149,51 @@ void getDifferenceRate(vector<string>& res, int start) {
     cout << "Average Diffs: " << avgDiffs << endl;
 }
 
-int main() {
-    srand(time(NULL));
-    vector<vector<int>> adjList = createTopology(TAXA);
-    int start = rand() % (adjList.size() - TAXA);
-    vector<string> res = generateData(adjList, start, LENGTH);
-    for (int i=res.size() - TAXA; i<res.size(); ++i) {
-        cout << "Species " << i - TAXA + 1 << ": " << res.at(i) << endl;
+// Creates and writes to file
+void createFile(unordered_map<int, string>& leaves) {
+    fstream myFile;
+    myFile.open("test-tree.nex", ios::out);
+    if (myFile.is_open()) {
+        myFile << "#NEXUS" << endl << endl;
+        myFile << "begin data;" << endl;
+        myFile << "\tdimensions ntax=" << TAXA << " nchar=" << LENGTH << ";" << endl;
+        myFile << "\tformat datatype=dna interleave=no gap=-;" << endl;
+        myFile << "\tmatrix" << endl;
+        for (auto it : leaves) {
+            myFile << "Node" << it.first << "\t" << it.second << endl;
+        }
+        myFile << "\t;" << endl;
+        myFile << "end;" << endl;
+        myFile.close();
+        return;
+    }
+    else {
+        cout << "error creating file" << endl;
+    }
+}
+
+// Prints adjacency list for testing
+void printTopology(vector<vector<int>>& adjList) {
+    for (int i=0; i<adjList.size(); ++i) {
+        cout << i << ": ";
+        for (int j=0; j<adjList.at(i).size(); ++j) {
+            cout << adjList.at(i).at(j) << " ";
+        }
         cout << endl;
     }
-    getDifferenceRate(res, start);
-    // DEBUG lmao sobbing and shitting rn
-    // for (int i=0; i<adjList.size(); ++i) {
-    //     cout << i << ": ";
-    //     for (int j=0; j<adjList.at(i).size(); ++j) {
-    //         cout << adjList.at(i).at(j) << " ";
-    //     }
-    //     cout << endl;
+}
+
+int main() {
+    srand(time(NULL));
+    vector<vector<int>> adjList = createTopology2(TAXA);
+    vector<string> res = generateData(adjList, LENGTH);
+    unordered_map<int, string> leaves = getLeaves(adjList, res);
+    createFile(leaves);
+    // cout << "TOPOLOGY:" << endl;
+    // printTopology(adjList);
+    // cout << "ALL DNA:" << endl;
+    // for (int i=0; i<res.size(); ++i) {
+    //     cout << i << ": " << res.at(i) << endl;
     // }
     return 0;
 }
