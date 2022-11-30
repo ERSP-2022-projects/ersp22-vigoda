85a0232a7337bbed2f03e7b87939b01a4679b2a8
Katy Tsao <64286635+katytsao@users.noreply.github.com>
2022-11-29 23:25:38 -0800

    so HOF was not necessary :(

--- treegen/main.cpp
+++ treegen/main.cpp
@@ -24,20 +24,17 @@ struct Edge {
     }
 };
 
-int roll_nucleotide(function<int(uint64_t*)> sm, int orig, double p_mutate, uint64_t* seed) {
-    if (nextFloat(seed) < p_mutate) return orig + sm(seed);
-    return orig;
-}
-
-int jc69(uint64_t* seed) {
-    return nextInt(seed, 4);
-}
-
-int k2p(uint64_t* seed) {
-    int a = nextInt(seed, 6);
-    if (a == 4) a = 0;
-    if (a == 5) a = 2;
-    return a;
+int roll_nucleotide(string smm, uint64_t* seed) {
+    if (smm == "k2p") {
+        int a = nextInt(seed, 6);
+        if (a == 4) a = 0;
+        if (a == 5) a = 2;
+        return a;
+    }
+    if (smm == "jc69") {
+        return nextInt(seed, 4);
+    }
+    return 0;
 }
 
 // note purines % 2 = 0, pyrimidines % 2 = 1
@@ -52,13 +49,14 @@ char convert_nucleotide(int id) {
 int main(int argc, char** argv) {
     // cmd line reading / initializations
     uint64_t seed = (argc >= 2) ? stoll(argv[1]) : (time(0) % 100000); // default seed = time % 1M
-    int species =     (argc == 5) ? stoi(argv[2]) : 10; // default # species = 10
-    int seq_length =  (argc == 5) ? stoi(argv[3]) : 1000; // default sequence length = 1000
-    double p_mutate = (argc == 5) ? stod(argv[4]) : 0.2; // default mutation probability = 0.2
+    int species = (argc >= 3) ? stoi(argv[2]) : 10; // default # species = 10
+    int seqlen = (argc >= 4) ? stoi(argv[3]) : 1000; // default sequence length = 1000
+    double p_mutate = (argc >= 5) ? stod(argv[4]) : 0.2; // default mutation probability = 0.2
+    string smm = (argc >= 6) ? argv[5] : "jc69"; // default site mutation model = jc69
     uint64_t orig = seed;
     Vertex vertices[2*species-2];
     Edge edges[2*species-3];
-    int sequences[2*species-2][seq_length];
+    int sequences[2*species-2][seqlen];
 
     // generate topology
     seed = init(orig + 1);
@@ -94,7 +92,7 @@ int main(int argc, char** argv) {
     bool visited[2*species-2];
     for (int i = 0; i < 2*species-2; i++) visited[i] = false;
     s.push(0); // starting node doesn't matter
-    for (int i = 0; i < seq_length; i++) 
+    for (int i = 0; i < seqlen; i++) 
         sequences[0][i] = nextInt(&seed, 4); // randomize 1st node
     while(!s.empty()) {
         int id = s.top();
@@ -106,9 +104,13 @@ int main(int argc, char** argv) {
                 s.push(v);
                 // mutate the sequence
                 // note id = ancestor, v = descendant
-                for (int i = 0; i < seq_length; i++)
-                    sequences[v][i] = 
-                        roll_nucleotide(jc69, sequences[id][i], p_mutate, &seed); // JC69
+                for (int i = 0; i < seqlen; i++) {
+                    sequences[v][i] = sequences[id][i];
+                    if (nextFloat(&seed) < p_mutate) {
+                        sequences[v][i] += roll_nucleotide(smm, &seed);
+                        sequences[v][i] %= 4;
+                    }
+                }
             }
         }
     }
@@ -116,13 +118,13 @@ int main(int argc, char** argv) {
     // write data to nex file
     file.open("results/data_" + to_string(orig) + ".nex");
     file << "begin data;" << endl;
-    file << "dimensions ntax=" << species << " nchar=" << seq_length << ";" << endl;
+    file << "dimensions ntax=" << species << " nchar=" << seqlen << ";" << endl;
     file << "format datatype=dna interleave=no gap=-;" << endl;
     file << "matrix" << endl;
     for (int v = 0; v < 2*species-2; v++) {
         if (!vertices[v].isLeaf) continue;
         file << vertices[v].name << "\t";
-        for (int i = 0; i < seq_length; i++)
+        for (int i = 0; i < seqlen; i++)
             file << convert_nucleotide(sequences[v][i]);
         file << endl;
     }
