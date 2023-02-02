bf1cffe4228a2a9898a5940171fca050b1289dbf
David Wang <david.wang5766@gmail.com>
2023-02-01 21:00:18 -0800

    changed treegen to use exportNewick function

--- treegen/treegen.cpp
+++ treegen/treegen.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <filesystem>
 #include "rng.h"
 #include "tree.h"
 #include "mutationmodels.h"
@@ -10,7 +11,8 @@ using namespace std;
 static const string SPECIES_NAMES[10] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 
 // note purines % 2 = 0, pyrimidines % 2 = 1
-char convert_nucleotide(int id) {
+char convert_nucleotide(int id)
+{
     if (id == 0)
         return 'A'; // pairs T, purine
     if (id == 1)
@@ -22,28 +24,37 @@ char convert_nucleotide(int id) {
     return '-';
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // initializations + cmd line reading
     uint64_t seed = (time(0) % 100000); // default seed = time % 1M
-    int species = 10; // default # species = 10
-    int seqlen = 1000; // default sequence length = 1000
-    double p_mutate = 0.2; // default mutation probability = 0.2
-    mutation_model smm = jc69; // default site mutation model = jc69
-    for (int i = 1; i < argc; i++) {
+    int species = 10;                   // default # species = 10
+    int seqlen = 1000;                  // default sequence length = 1000
+    double p_mutate = 0.2;              // default mutation probability = 0.2
+    mutation_model smm = jc69;          // default site mutation model = jc69
+    for (int i = 1; i < argc; i++)
+    {
         string arg = argv[i];
         int eq = arg.find('=');
-        if (eq == -1) {
+        if (eq == -1)
+        {
             cerr << "argument \'" << arg << "\" is not a valid parameter assignment" << endl;
             continue;
         }
         string param = arg.substr(0, eq);
-        string after = arg.substr(eq+1);
-        if (param == "seed") seed = stoll(after);
-        else if (param == "species" || param == "numspecies") species = stoi(after);
-        else if (param == "seqlen") seqlen = stoi(after);
-        else if (param == "p_mutate") p_mutate = stod(after);
-        else if (param == "smm" || param == "mutation_model") smm = stomm(after);
-        else cerr << param << " is not a valid parameter" << endl;
+        string after = arg.substr(eq + 1);
+        if (param == "seed")
+            seed = stoll(after);
+        else if (param == "species" || param == "numspecies")
+            species = stoi(after);
+        else if (param == "seqlen")
+            seqlen = stoi(after);
+        else if (param == "p_mutate")
+            p_mutate = stod(after);
+        else if (param == "smm" || param == "mutation_model")
+            smm = stomm(after);
+        else
+            cerr << param << " is not a valid parameter" << endl;
     }
     uint64_t orig = seed;
     vector<Vertex> vertices(2 * species - 2);
@@ -55,7 +66,8 @@ int main(int argc, char **argv) {
     vertices[0] = Vertex(SPECIES_NAMES[0]);
     vertices[1] = Vertex(SPECIES_NAMES[1]);
     edges[0] = Edge(0, 1);
-    for (int i = 2; i < species; i++) {
+    for (int i = 2; i < species; i++)
+    {
         vertices[2 * i - 2] = Vertex(SPECIES_NAMES[i]);
         vertices[2 * i - 1] = Vertex();
         edges[2 * i - 3] = Edge(2 * i - 2, 2 * i - 1);
@@ -65,26 +77,25 @@ int main(int argc, char **argv) {
     }
 
     // log tree info to txt file
-    ofstream file;
-    file.open("results/tree_" + to_string(orig) + ".txt");
-    for (int i = 0; i < 2 * species - 3; i++) {
-        Edge e = edges[i];
-        if ((vertices[e.v1].isLeaf))
-            file << vertices[e.v1].name;
-        else
-            file << e.v1;
-        file << ", ";
-        if ((vertices[e.v2].isLeaf))
-            file << vertices[e.v2].name;
-        else
-            file << e.v2;
-        file << endl;
-    }
-    file.close();
+
+    // file.open("results/tree_" + to_string(orig) + ".txt");
+    // for (int i = 0; i < 2 * species - 3; i++) {
+    //     Edge e = edges[i];
+    //     if ((vertices[e.v1].isLeaf))
+    //         file << vertices[e.v1].name;
+    //     else
+    //         file << e.v1;
+    //     file << ", ";
+    //     if ((vertices[e.v2].isLeaf))
+    //         file << vertices[e.v2].name;
+    //     else
+    //         file << e.v2;
+    //     file << endl;
+    // }
+    // file.close();
 
     NewickTree newickFormatted(vertices, edges, 3);
-    newickFormatted.printNewick();
-
+    newickFormatted.printNewick(true);
     //  dfs sequence generation
     seed = init(orig + 2);
     stack<int> s;
@@ -94,19 +105,24 @@ int main(int argc, char **argv) {
     s.push(0); // starting node doesn't matter
     for (int i = 0; i < seqlen; i++)
         sequences[0][i] = nextInt(&seed, 4); // randomize 1st node
-    while (!s.empty()) {
+    while (!s.empty())
+    {
         int id = s.top();
         visited[id] = true;
         s.pop();
-        for (int i = 0; i < 2 * species - 3; i++) {
+        for (int i = 0; i < 2 * species - 3; i++)
+        {
             int v = edges[i].has(id);
-            if (v != -1 && !visited[v]) {
+            if (v != -1 && !visited[v])
+            {
                 s.push(v);
                 // mutate the sequence
                 // note id = ancestor, v = descendant
-                for (int i = 0; i < seqlen; i++) {
+                for (int i = 0; i < seqlen; i++)
+                {
                     sequences[v][i] = sequences[id][i];
-                    if (nextFloat(&seed) < p_mutate) {
+                    if (nextFloat(&seed) < p_mutate)
+                    {
                         sequences[v][i] += roll_nucleotide(smm, &seed);
                         sequences[v][i] %= 4;
                     }
@@ -116,13 +132,17 @@ int main(int argc, char **argv) {
     }
 
     // write data to nex file
-    file.open("results/data_" + to_string(orig) + ".nex");
+    filesystem::create_directory("results/" + to_string(orig));
+    ofstream file;
+    file.open("results/" + to_string(orig) + "/data.nex");
     file << "begin data;" << endl;
     file << "dimensions ntax=" << species << " nchar=" << seqlen << ";" << endl;
     file << "format datatype=dna interleave=no gap=-;" << endl;
     file << "matrix" << endl;
-    for (int v = 0; v < 2 * species - 2; v++) {
-        if (!vertices[v].isLeaf) continue;
+    for (int v = 0; v < 2 * species - 2; v++)
+    {
+        if (!vertices[v].isLeaf)
+            continue;
         file << vertices[v].name << "\t";
         for (int i = 0; i < seqlen; i++)
             file << convert_nucleotide(sequences[v][i]);
@@ -131,6 +151,7 @@ int main(int argc, char **argv) {
     file << ";" << endl;
     file << "end;" << endl;
     file.close();
-
+    newickFormatted.exportNewick("results/" + to_string(orig) + "/tree.txt", true);
+    cout << "exported results to " << to_string(orig) << endl;
     return 0;
 }
