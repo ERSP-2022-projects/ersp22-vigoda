c205c53f2b498f1dfacfaa53470193c1479a2756
David Wang <david.wang5766@gmail.com>
2023-02-01 20:58:41 -0800

    added functionality to generate sequences on each node and export leaf node sequences to nexus file

--- treegen/NewickTree.cpp
+++ treegen/NewickTree.cpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 #include <stack>
 #include <map>
+#include <set>
+#include <random>
+#include <stdexcept>
 using namespace std;
 
 NewickTree::NewickTree()
@@ -14,10 +17,14 @@ NewickTree::NewickTree()
 }
 NewickTree::NewickTree(string rootName)
 {
+    sequenceLength = 0;
+    transitionMatrix = map<char, map<char, double>>();
     root = new NewickTree::TreeNode(rootName, nullptr);
 }
 NewickTree::NewickTree(vector<Vertex> vertices, vector<Edge> edges, int rootIndex)
 {
+    sequenceLength = 0;
+    transitionMatrix = map<char, map<char, double>>();
     vector<string> names(vertices.size());
     for (int i = 0; i < names.size(); i++)
     {
@@ -30,32 +37,18 @@ NewickTree::NewickTree(vector<Vertex> vertices, vector<Edge> edges, int rootInde
         adj[e.v2].push_back(e.v1);
     }
     // should check that adj and names are same size and rootIndex < size later
-    vector<bool> visited(adj.size());
-    root = new NewickTree::TreeNode(names[rootIndex], nullptr);
-    queue<pair<NewickTree::TreeNode *, int>> next;
-    visited[rootIndex] = true;
-    next.push(make_pair(root, rootIndex));
-    while (next.size() > 0)
-    {
-        NewickTree::TreeNode *currNode = next.front().first;
-        int currIndex = next.front().second;
-        next.pop();
-        for (int neighborIndex : adj[currIndex])
-        {
-            if (!visited[neighborIndex])
-            {
-                // add as child of curr, mark as visited, add to queue
-                NewickTree::TreeNode *neighborNode = new NewickTree::TreeNode(names[neighborIndex], currNode);
-                currNode->children.push_back(neighborNode);
-                visited[neighborIndex] = true;
-                next.push(make_pair(neighborNode, neighborIndex));
-            }
-        }
-    }
+    adjToTree(adj, names, rootIndex);
 }
 NewickTree::NewickTree(vector<vector<int>> adj, vector<string> names, int rootIndex)
 {
     // should check that adj and names are same size and rootIndex < size later
+    sequenceLength = 0;
+    transitionMatrix = map<char, map<char, double>>();
+    adjToTree(adj, names, rootIndex);
+}
+
+void NewickTree::adjToTree(vector<vector<int>> adj, vector<string> names, int rootIndex)
+{
     vector<bool> visited(adj.size());
     root = new NewickTree::TreeNode(names[rootIndex], nullptr);
     queue<pair<NewickTree::TreeNode *, int>> next;
@@ -71,8 +64,7 @@ NewickTree::NewickTree(vector<vector<int>> adj, vector<string> names, int rootIn
             if (!visited[neighborIndex])
             {
                 // add as child of curr, mark as visited, add to queue
-                NewickTree::TreeNode *neighborNode = new NewickTree::TreeNode(names[neighborIndex], currNode);
-                currNode->children.push_back(neighborNode);
+                NewickTree::TreeNode *neighborNode = currNode->addChild(names[neighborIndex]);
                 visited[neighborIndex] = true;
                 next.push(make_pair(neighborNode, neighborIndex));
             }
@@ -115,6 +107,11 @@ void NewickTree::newickDFS(NewickTree::TreeNode *start, vector<string> &symbols,
     if (!leafOnly || start->isLeaf())
         symbols.push_back(start->name);
 }
+int NewickTree::getSequenceLength()
+{
+    return sequenceLength;
+}
+
 void NewickTree::printNewick(bool leafOnly)
 {
     vector<string> symbols;
@@ -141,9 +138,9 @@ void NewickTree::exportNewick(string filename, bool leafOnly)
 NewickTree::TreeNode *NewickTree::importDFS(TreeNode *parent, string &newickString, int &pos)
 {
     // pos should be position before current node
-    cout << "running at " << pos << endl;
+    // cout << "running at " << pos << endl;
     int nextEnd = newickString.find_first_of("(,)", pos + 1);
-    cout << "found nextEnd at " << nextEnd << " which is " << newickString.at(nextEnd) << endl;
+    // cout << "found nextEnd at " << nextEnd << " which is " << newickString.at(nextEnd) << endl;
     string currName;
     if (nextEnd == pos + 1)
     {
@@ -155,25 +152,22 @@ NewickTree::TreeNode *NewickTree::importDFS(TreeNode *parent, string &newickStri
         reverse(currName.begin(), currName.end());
     }
     TreeNode *currNode = new TreeNode(currName, parent);
-    if (parent != nullptr)
-        cout << "creating " << currName << " under " << parent->name << endl;
+    // if (parent != nullptr)
+    //     cout << "creating " << currName << " under " << parent->name << endl;
     pos = nextEnd;
     if (newickString.at(nextEnd) == ')')
     {
         while (newickString.at(pos) != '(')
         {
-            currNode->children.push_back(importDFS(currNode, newickString, pos));
+            currNode->addChild(importDFS(currNode, newickString, pos));
         }
         pos += 1;
     }
-    cout << "returning at pos " << pos << endl;
     return currNode;
 }
 void NewickTree::importNewick(string filename)
 {
-    cout << "got hereee!" << endl;
     deleteDFS(root);
-    cout << "got here!!" << endl;
     ifstream treefile;
     treefile.open(filename);
     string newickString;
@@ -184,4 +178,251 @@ void NewickTree::importNewick(string filename)
     cout << newickString << endl;
     root = importDFS(nullptr, newickString, pos);
     return;
+}
+int NewickTree::getLeafCount()
+{
+    queue<TreeNode *> next;
+    int leafCount = 0;
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    next.push(root);
+    while (next.size() > 0)
+    {
+        TreeNode *currNode = next.front();
+        next.pop();
+        if (currNode->isLeaf())
+        {
+            leafCount++;
+        }
+        for (TreeNode *child : currNode->children)
+        {
+            next.push(child);
+        }
+    }
+    return leafCount;
+}
+void NewickTree::setTransition(map<char, map<char, double>> transitionMatrix)
+{
+
+    if (transitionMatrix.size() == 0)
+    {
+        double p_mutate = 0.3;
+        transitionMatrix = map<char, map<char, double>>{
+            {'A', {{'A', 1.0 - p_mutate}, {'T', p_mutate / 3.0}, {'G', p_mutate / 3.0}, {'C', p_mutate / 3.0}}},
+            {'T', {{'A', p_mutate / 3.0}, {'T', 1.0 - p_mutate}, {'G', p_mutate / 3.0}, {'C', p_mutate / 3.0}}},
+            {'G', {{'A', p_mutate / 3.0}, {'T', p_mutate / 3.0}, {'G', 1.0 - p_mutate}, {'C', p_mutate / 3.0}}},
+            {'C', {{'A', p_mutate / 3.0}, {'T', p_mutate / 3.0}, {'G', p_mutate / 3.0}, {'C', 1.0 - p_mutate}}}};
+    }
+    this->transitionMatrix = transitionMatrix;
+}
+void NewickTree::generateSequences(int sequenceLength)
+{
+    if (transitionMatrix.size() != symbols.size())
+    {
+        setTransition();
+        cerr << "dimensions of transitionMatrix are incorrect, setting transitionMatrix to default (mutation probability of 0.3 equally split)" << endl;
+    }
+    for (pair<char, map<char, double>> mapping : transitionMatrix)
+    {
+        if (mapping.second.size() != symbols.size())
+        {
+            setTransition();
+            cerr << "dimensions of transitionMatrix are incorrect, setting transitionMatrix to default (mutation probability of 0.3 equally split)" << endl;
+            break;
+        }
+    }
+    this->sequenceLength = sequenceLength;
+    mt19937 mt(time(nullptr));
+    default_random_engine gen;
+    uniform_real_distribution<double> distribution(0.0,
+                                                   1.0);
+    string rootSeq = "";
+    for (int i = 0; i < sequenceLength; i++)
+    {
+        char newChar = symbols[mt() % symbols.size()];
+        rootSeq.push_back(newChar);
+    }
+    root->sequence = rootSeq;
+    queue<TreeNode *> next;
+    next.push(root);
+    while (next.size() > 0)
+    {
+        TreeNode *currNode = next.front();
+        next.pop();
+        for (TreeNode *child : currNode->children)
+        {
+            string childSeq = "";
+            for (int i = 0; i < currNode->sequence.size(); i++)
+            {
+                char nucleotide = currNode->sequence.at(i);
+                double randChoice = distribution(gen);
+                map<char, double> probabilities = transitionMatrix[nucleotide];
+                double cummSum = 0.0;
+                for (pair<char, double> candidate : probabilities)
+                {
+                    cummSum += candidate.second;
+                    if (cummSum >= randChoice)
+                    {
+                        cout << nucleotide << " " << candidate.first << " " << cummSum << " " << candidate.second << endl;
+                        childSeq.push_back(candidate.first);
+                        break;
+                    }
+                }
+                // deals with floating point errors that cause transition probabilities to not sum up to 1 (should also manually check that they sum to 1 in each row)
+                if (childSeq.size() < i + 1)
+                {
+                    childSeq.push_back(probabilities.rbegin()->first);
+                }
+            }
+            child->sequence = childSeq;
+            next.push(child);
+        }
+        // if (currNode->isLeaf())
+        // {
+        //     cout << currNode->name << " " << currNode->sequence << endl;
+        // }
+    }
+}
+void NewickTree::toNexus(string filename, map<string, string> sequences)
+{
+    ofstream file;
+    file.open(filename);
+    file << "begin data;" << endl;
+    file << "dimensions ntax=" << getLeafCount() << " nchar=" << sequenceLength << ";" << endl;
+    file << "format datatype=dna interleave=no gap=-;" << endl;
+    file << "matrix" << endl;
+
+    for (pair<string, string> sequence : sequences)
+    {
+        file << sequence.first << "\t" << sequence.second << endl;
+    }
+
+    file << ";" << endl;
+    file << "end;" << endl;
+    file.close();
+}
+map<string, string> NewickTree::getSequences()
+{
+    queue<TreeNode *> next;
+    map<string, string> sequences;
+    if (root == nullptr)
+    {
+        return sequences;
+    }
+    next.push(root);
+    while (next.size() > 0)
+    {
+        TreeNode *currNode = next.front();
+        next.pop();
+        if (currNode->isLeaf())
+        {
+            sequences[currNode->name] = currNode->sequence;
+        }
+        for (TreeNode *child : currNode->children)
+        {
+            next.push(child);
+        }
+    }
+    return sequences;
+}
+void NewickTree::exportNexus(string filename)
+{
+    toNexus(filename, getSequences());
+}
+map<string, string> NewickTree::mixtureModel(vector<NewickTree> trees, vector<double> weights = vector<double>())
+{
+    // validate that trees is not empty
+    if (trees.size() == 0)
+    {
+        throw invalid_argument("NewickTree::mixtureModel 'trees' must be non emptu vector of type NewickTree");
+    }
+    // validate that weights is the same length as trees or is 0
+    if (weights.size() != 0 && weights.size() != trees.size())
+    {
+        throw invalid_argument("NewickTree::mixtureModel 'weights' vector must be either empty or of the same dimensions as 'trees'");
+    }
+    // validate that all trees have the same leaves
+    int leafCount = trees[0].getLeafCount();
+    for (NewickTree tree : trees)
+    {
+        if (tree.getLeafCount() != leafCount)
+        {
+            throw invalid_argument("NewickTree::mixtureModel all NewickTree's in 'trees' vector must have the same number of leaf nodes");
+        }
+    }
+    vector<map<string, string>> sequenceList(trees.size());
+    for (int i = 0; i < trees.size(); i++)
+    {
+        sequenceList[i] = trees[i].getSequences();
+    }
+    set<string> keySet;
+    for (pair<string, string> keyPair : sequenceList[0])
+    {
+        keySet.insert(keyPair.first);
+    }
+    for (int i = 0; i < trees.size(); i++)
+    {
+        for (pair<string, string> keyPair : sequenceList[i])
+        {
+            if (keySet.find(keyPair.first) == keySet.end())
+            {
+                throw invalid_argument("NewickTree::mixtureModel all NewickTree's in 'trees' vector must have identically labeled leaf nodes");
+            }
+        }
+    }
+
+    // validate that all trees have equal sequence lengths
+    int sequenceLength = trees[0].getSequenceLength();
+    for (NewickTree tree : trees)
+    {
+        if (tree.getSequenceLength() != sequenceLength)
+        {
+            throw invalid_argument("NewickTree::mixtureModel all NewickTree's in 'trees' vector must have the same sequence length");
+        }
+    }
+
+    if (weights.size() == 0)
+    {
+        weights = vector<double>(trees.size());
+        for (int i = 0; i < weights.size(); i++)
+        {
+            weights[i] = 1.0 / weights.size();
+        }
+    }
+    default_random_engine gen;
+    uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    map<string, string> mixedSequence;
+    for (string key : keySet)
+    {
+        mixedSequence[key] = "";
+    }
+    for (int i = 0; i < sequenceLength; i++)
+    {
+        // pick random tree for this index
+        double randChoice = distribution(gen);
+        int randIndex = -1;
+        double cummSum = 0.0;
+        for (int j = 0; j < weights.size(); j++)
+        {
+            cummSum += weights[j];
+            if (cummSum >= randChoice)
+            {
+                randIndex = j;
+                break;
+            }
+        }
+        if (randIndex == -1)
+        {
+            cerr << "Did not properly select random index for position " << i << " in mixture model. Defaulting to " << weights.size() - 1 << "." << endl;
+            randIndex = weights.size() - 1;
+        }
+        for (string key : keySet)
+        {
+            mixedSequence[key].push_back(sequenceList[randIndex][key].at(i));
+        }
+    }
+    return mixedSequence;
 }
--- treegen/NewickTree.h
+++ treegen/NewickTree.h
@@ -4,6 +4,8 @@
 #include "tree.h"
 #include <string>
 #include <vector>
+#include <utility>
+#include <map>
 using namespace std;
 
 class NewickTree
@@ -11,13 +13,21 @@ class NewickTree
     struct TreeNode
     {
         string name;
+        string sequence;
         TreeNode *parent;
         vector<TreeNode *> children;
-        TreeNode() {}
-        TreeNode(string n, TreeNode *p) : name(n), parent(p) {}
-        void addChild(string n)
+        TreeNode() : name(""), sequence(""), parent(nullptr), children(vector<TreeNode *>(0)) {}
+        TreeNode(string n, TreeNode *p) : name(n), sequence(""), parent(p), children(vector<TreeNode *>(0)) {}
+        TreeNode *addChild(string n)
         {
-            children.push_back(new TreeNode(n, this));
+            TreeNode *newNode = new TreeNode(n, this);
+            children.push_back(newNode);
+            return newNode;
+        }
+        TreeNode *addChild(TreeNode *child)
+        {
+            children.push_back(child);
+            return child;
         }
         int childrenCount()
         {
@@ -30,9 +40,14 @@ class NewickTree
     };
 
 private:
+    int sequenceLength;
     TreeNode *root;
+    map<char, map<char, double>> transitionMatrix;
+    inline static vector<char> symbols = {'A', 'T', 'G', 'C'};
+    void adjToTree(vector<vector<int>> adj, vector<string> names, int rootIndex);
     void newickDFS(TreeNode *start, vector<string> &symbols, bool leafOnly);
     void deleteDFS(TreeNode *start);
+    void toNexus(string filename, map<string, string> sequences);
     TreeNode *importDFS(TreeNode *parent, string &newickString, int &pos);
 
 public:
@@ -41,9 +56,16 @@ public:
     NewickTree(vector<Vertex> vertices, vector<Edge> edges, int rootIndex);
     NewickTree(vector<vector<int>> adj, vector<string> names, int rootIndex = 0);
     ~NewickTree();
+    int getSequenceLength();
+    int getLeafCount();
+    void generateSequences(int sequenceLength);
     void printNewick(bool leafOnly = true);
     void exportNewick(string filename, bool leafOnly = true);
     void importNewick(string filename);
+    void exportNexus(string filename);
+    void setTransition(map<char, map<char, double>> transitionMatrix = map<char, map<char, double>>());
+    map<string, string> getSequences();
+    static map<string, string> mixtureModel(vector<NewickTree> trees, vector<double> weights);
 };
 
 #endif
