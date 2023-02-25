ebee431bbc87d6e7b9f1682c5faf6357a7422793
Yash Vangala <yvangala@ucsb.edu>
2023-02-24 22:44:50 -0800

    it works! now I can automate!

--- treeanalysis/mrBayes.py
+++ treeanalysis/mrBayes.py
@@ -11,16 +11,48 @@ def generate_mrbayes_script(input_filename, output_filename):
         script_file.write(script)
         
 
-def treegen(species = 10,seqlen = 1000,p_mutate = 0.2 ,mutation_model = "jc69",seed=None):
+# def treegen(species = 10,seqlen = 1000,p_mutate = 0.2 ,mutation_model = "jc69",seed=None):
     
+#     # set the path to the C++ executable and the command-line arguments
+#     executable_path ="C:/Users/yasha/Github/ersp22-vigoda/treegen/treegen.out"
+    
+#     command = [executable_path, f"species={species}", f"seqlen={seqlen}", 
+#         f"p_mutate={p_mutate}", f"mutation_model={mutation_model}"]    
+#     if(seed is not None):
+#         command.append(f"seed={seed}")
+#     # run the C++ executable with the specified arguments
+#     #print(command)
+#     process = subprocess.run(command, capture_output=True, text=True)
+#     #print the output of the C++ executable
+#     print(f"{process.stdout=}")
+#     print(f"{process.stderr=}")
+    
+def treegen(species=10, seqlen=1000, p_mutate=0.2, mutation_model="jc69", seed=None):
     # set the path to the C++ executable and the command-line arguments
-    executable_path ="C:/Users/yasha/Github/ersp22-vigoda/treegen/treegen.out"
+    executable_path = "./treegen.out"
+    result_directory = "C:/Users/yasha/Github/ersp22-vigoda/treegen/result"
     
-    command = f"{executable_path} {species=} {seqlen=} {p_mutate=} {mutation_model=} "
-    if(seed is not None):
-        command.append(f"{seed=}")
+    # set the working directory to the location of the C++ executable
+    os.chdir(os.path.dirname(executable_path))
+    
+    command = [os.path.basename(executable_path), f"species={species}", f"seqlen={seqlen}", f"p_mutate={p_mutate}", f"mutation_model={mutation_model}"]
+    if seed is not None:
+        command.append(f"seed={seed}")
     # run the C++ executable with the specified arguments
-    #print(command)
-    process = subprocess.run(command, shell=True, capture_output=True, text=True)
-    # print the output of the C++ executable
     
+    #print(" ".join(command))
+    
+    # process = subprocess.run(["echo","works!"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)    # print the output of the C++ executable
+    process = subprocess.run(["./treegen.out"], capture_output=True, text=True)
+
+    if process.stdout != "":
+        print(f"{process.stdout=}")
+    if process.stderr != "":
+        print(f"{process.stderr=}")
+    
+    # change the working directory back to the original location
+    os.chdir(os.path.dirname(os.getcwd()))
+
+
+if __name__ == "__main__":
+    treegen()
