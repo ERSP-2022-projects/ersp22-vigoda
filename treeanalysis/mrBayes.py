7c2b1c938200d2c9bc54ace10b102b148b5d58f8
Yash Vangala <yvangala@ucsb.edu>
2023-02-27 18:31:43 -0800

    script almsot done need to go to physics class

--- treeanalysis/mrBayes.py
+++ treeanalysis/mrBayes.py
@@ -3,8 +3,36 @@ import subprocess
 import shutil
 
 
-def generate_mrbayes_script(nexus_filepath, output_filepath=None, same_directory=True):
+def generate_mrbayes_script(nexus_filepath, output_filepath=None, ngen=50000, samplefreq=100, nchains=1, nruns=2, printfreq=1000, diagnfreq=1000, burnin=250, nst=6, rates="gamma", addToNexusFile=True):
+    input_filename = os.path.basename(nexus_filepath)
+    prefix = input_filename.split("_")[0]
+
+    # Create the directory inside the analysis folder
+    analysis_dir = os.path.join(
+        "C:/Users/yasha/Github/ersp22-vigoda/treeanalysis/analysis", prefix)
+    os.makedirs(analysis_dir, exist_ok=True)
+
+    src_dir = os.path.dirname(nexus_filepath)
+    dest_dir = analysis_dir
+    for file_name in os.listdir(src_dir):
+        # Check if the file starts with the prefix "input_filename"
+        if file_name.startswith(prefix):
+            # If the file starts with the prefix, construct the source and destination file paths
+            src_path = os.path.join(src_dir, file_name)
+            dest_path = os.path.join(dest_dir, file_name)
+            # Check if the file already exists in the destination directory
+            if os.path.exists(dest_path):
+                print(
+                    f"File '{file_name}' already exists in the destination directory, skipping...")
+            else:
+                # If the file does not exist in the destination directory, move it there
+                shutil.move(src_path, dest_path)
+    nexus_filepath = os.path.join(analysis_dir, input_filename)
+    if output_filepath != None:
+        output_filename = os.path.basename(output_filepath)
+        output_filepath = os.path.join(analysis_dir, output_filename)
     TEMPLATE_PATH = r"C:\Users\yasha\Github\ersp22-vigoda\treeanalysis\mrbayes_template.nexus"
+
     temp_path = os.path.normpath(TEMPLATE_PATH)
 
     nexus_filepath = os.path.normpath(nexus_filepath)
@@ -12,15 +40,32 @@ def generate_mrbayes_script(nexus_filepath, output_filepath=None, same_directory
         filename = os.path.basename(nexus_filepath)
         output_filepath = os.path.join(os.path.dirname(
             nexus_filepath), f"{filename}.mrbayes")
-        # print(f"{(nexus_filepath,output_filepath)}")
     output_filepath = os.path.normpath(output_filepath)
     with open(temp_path, "r") as template_file:
         template = template_file.read()
-    script = template.replace("<input_filename>", nexus_filepath)
+    script = template
+    if addToNexusFile == False:
+        script = script.replace("<input_filename>", nexus_filepath)
     log_filename = os.path.basename(nexus_filepath)
     script = script.replace("<log_filename>", log_filename)
-    with open(output_filepath, "w") as script_file:
-        script_file.write(script)
+    script = script.replace("<ngen>", str(ngen))
+    script = script.replace("<samplefreq>", str(samplefreq))
+    script = script.replace("<nchains>", str(nchains))
+    script = script.replace("<nruns>", str(nruns))
+    script = script.replace("<printfreq>", str(printfreq))
+    script = script.replace("<diagnfreq>", str(diagnfreq))
+    script = script.replace("<burnin>", str(burnin))
+    script = script.replace("<nst>", str(nst))
+    script = script.replace("<rates>", str(rates))
+
+    if addToNexusFile:
+        with open(nexus_filepath, "a") as nexus_file:
+            nexus_file.write(
+                "\n" + script.replace("execute <input_filename>;", "") + "\n")
+            output_filepath = nexus_filepath
+    else:
+        with open(output_filepath, "w") as script_file:
+            script_file.write(script)
     return output_filepath
 
 
@@ -36,48 +81,11 @@ def run_mrbayes(script_filepath, directory=None):
     return (process.stdout, process.stderr)
 
 
-def NexusToRun(nexus_filepath, script_filepath=None):
-    nexus_filepath = os.path.normpath(nexus_filepath)
-
-    if script_filepath == None:
-        script_filepath = generate_mrbayes_script(nexus_filepath)
-    script_filepath = os.path.normpath(script_filepath)
-
-    output, error = run_mrbayes(script_filepath)
-
-    input_filename = os.path.basename(nexus_filepath)
-    prefix = input_filename.split("_")[0]
-
-    # Create the directory inside the analysis folder
-    analysis_dir = os.path.join(
-        "C:/Users/yasha/Github/ersp22-vigoda/treeanalysis/analysis", prefix)
-    os.makedirs(analysis_dir, exist_ok=True)
-
-    src_dir = os.path.dirname(nexus_filepath)
-    dest_dir = analysis_dir
-
-    # Iterate over all files in the source directory
-    for file_name in os.listdir(src_dir):
-        # Check if the file starts with the prefix "input_filename"
-        if file_name.startswith(prefix):
-            # If the file starts with the prefix, construct the source and destination file paths
-            src_path = os.path.join(src_dir, file_name)
-            dest_path = os.path.join(dest_dir, file_name)
-            # Check if the file already exists in the destination directory
-            if os.path.exists(dest_path):
-                print(
-                    f"File '{file_name}' already exists in the destination directory, skipping...")
-            else:
-                # If the file does not exist in the destination directory, move it there
-                shutil.move(src_path, dest_path)
-
-    return (output, error)
-
-
 def treegen(species=10, seqlen=1000, p_mutate=0.2, mutation_model="jc69", seed=None):
     # set the path to the C++ executable and the command-line arguments
     executable_path = "./treegen.out"
     treegen_directory = "C:/Users/yasha/Github/ersp22-vigoda/treegen"
+    results_directory = "C:/Users/yasha/Github/ersp22-vigoda/treegen/results"
     old_directory = os.getcwd()
     # set the working directory to the location of the C++ executable
     os.chdir(treegen_directory)
@@ -88,14 +96,50 @@ def treegen(species=10, seqlen=1000, p_mutate=0.2, mutation_model="jc69", seed=N
                f"mutation_model={mutation_model}"]
     if seed is not None:
         command.append(f"seed={seed}")
-    # process = subprocess.run(["echo","works!"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)    # print the output of the C++ executable
     process = subprocess.run(["./treegen.out"], capture_output=True, text=True)
+
+    # get the prefix of the newly created NEXUS file
+    prefix = (process.stdout).split('\n')[-2].split()[-1]
 
     # change the working directory back to the original location
     os.chdir(old_directory)
-    return (process.stdout, process.stderr)
+
+    for filename in os.listdir(results_directory):
+        if filename.startswith(prefix) and filename.endswith('.nex'):
+            return os.path.join(results_directory, filename)
+
+    raise FileNotFoundError(
+        f"The file with prefix: '{prefix}' does not exist in results directory")
+
+
+def generateAndRun(**kwargs):
+    treegen_params = {
+        'species': kwargs['species'],
+        'seqlen': kwargs['seqlen'],
+        'p_mutate': kwargs['p_mutate'],
+        'mutation_model': kwargs['mutation_model'],
+        'seed': kwargs['seed'],
+    }
+    nexus_filepath = treegen(**treegen_params)
+    generate_mrbayes_script_params = {
+        'nexus_filepath': nexus_filepath,
+        'ngen': kwargs['ngen'],
+        'samplefreq': kwargs['samplefreq'],
+        'nchains': kwargs['nchains'],
+        'nruns': kwargs['nruns'],
+        'printfreq': kwargs['printfreq'],
+        'diagnfreq': kwargs['diagnfreq'],
+        'burnin': kwargs['burnin'],
+        'nst': kwargs['nst'],
+        'rates': kwargs['rates'],
+        'addToNexusFile': kwargs['addToNexusFile'],
+    }
+
+    script_filepath = generate_mrbayes_script(**generate_mrbayes_script_params)
+    return run_mrbayes(script_filepath)
 
 
 if __name__ == "__main__":
-    path = "C:\\Users\\yasha\\Github\\ersp22-vigoda\\treegen\\results\\44350_data.nex"
-    print(NexusToRun(path))
+    # path = "C:\\Users\\yasha\\Github\\ersp22-vigoda\\treegen\\results\\45299_data.nex"
+    # print(NexusToRun(path))
+    print(generateAndRun())
